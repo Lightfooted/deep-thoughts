98ddc3baffec13fe1702228a692ab537412f0ccf
Lightfooted <vesselofbalance@hotmail.com>
2021-11-08 23:26:33 -0500

    figured out blank issue. new version of react router no longer uses component. write as element instead.

--- client/src/App.js
+++ client/src/App.js
@@ -27,12 +27,12 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile/:username?" component={Profile} />
-              <Route exact path="/thought/:id" component={SingleThought} />
-              <Route component={NoMatch} />
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/profile/:username?" element={<Profile />} />
+              <Route exact path="/thought/:id" element={<SingleThought />} />
+              <Route element={<NoMatch />} />
             </Routes>
           </div>
           <Footer />
