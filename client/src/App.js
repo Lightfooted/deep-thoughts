00d0c2a9716f03837fc625887cf38dde06cb28fa
Lightfooted <vesselofbalance@hotmail.com>
2021-11-08 22:43:58 -0500

    installed react router dom (my favorite) used routes instead of switch because of newest version. routes work but pages are blank other than footer and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import React from 'react';
-
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
@@ -18,13 +22,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
-      </div>
-      <Footer />
-    </div>
+      <BrowserRouter>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
+              <Route component={NoMatch} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
